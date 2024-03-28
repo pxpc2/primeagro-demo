1f6f4704d29dd15e46c859fc8e1d9ed241853b82
Pedro Daia <pedrodaia.c@gmail.com>
2024-03-28 00:32:11 -0300

    fixing submit-button component

--- app/login/submit-button.jsx
+++ app/login/submit-button.jsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { useState } from "react";
 import { useFormStatus } from "react-dom";
 
 export function SubmitButton({ children, pendingText, ...props }) {
   const { pending, action } = useFormStatus();
 
-  const [isPending, setIsPending] = useState(false);
-
-  const handleClick = async () => {
-    setIsPending(true);
-    await props.formAction();
-    setIsPending(false);
-  };
+  const isPending = pending && action === props.formAction;
 
   return (
-    <button
-      {...props}
-      type="submit"
-      aria-disabled={pending || isPending}
-      onClick={handleClick}
-    >
+    <button {...props} type="submit" aria-disabled={pending}>
       {isPending ? pendingText : children}
     </button>
   );
