351b6615dcc395ebf4a57080cccb1ed08ad1850b
Pedro Daia <pedrodaia.c@gmail.com>
2024-05-11 14:55:03 -0300

    under construction warning

--- app/page.js
+++ app/page.js
@@ -3,10 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="w-full h-full flex items-center align-middle justify-center">
+    <main className="w-full h-full flex items-center flex-col align-middle justify-center">
+      <h1 className="font-bold text-4xl pb-12">EM CONSTRUÇÃO.</h1>
       <Link href={"/login"}>
         <button className="rounded-md bg-white px-28 py-5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Acessar sistema PrimeAgro
+          Sistema ConfidensAgro
         </button>
       </Link>
     </main>
