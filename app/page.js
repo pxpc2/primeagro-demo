992d7ac0043564289ac5a33e9e833f8487676bf0
Pedro Daia <pedrodaia.c@gmail.com>
2024-06-11 00:02:35 -0300

    direct to login

--- app/page.js
+++ app/page.js
@@ -1,15 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="w-full h-full flex items-center flex-col align-middle justify-center">
-      <h1 className="font-bold text-4xl pb-12">EM CONSTRUÇÃO.</h1>
-      <Link href={"/login"}>
-        <button className="rounded-md bg-white px-28 py-5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          Sistema ConfidensAgro
-        </button>
-      </Link>
-    </main>
-  );
+  return redirect("/login");
 }
