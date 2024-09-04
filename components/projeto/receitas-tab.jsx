61c94b80c470a4b90af99efbe56c5561d2f8e1af
Pedro Daia <pedrodaia.c@gmail.com>
2024-09-04 00:11:27 -0300

    adding cards for receitas tab

--- components/projeto/receitas-tab.jsx
+++ components/projeto/receitas-tab.jsx
@@ -10,26 +10,33 @@ import {
   TableRow,
 } from "../ui/table";
 import { Input } from "../ui/input";
-import { ANO_INICIAL } from "@/utils/constants";
+import {
+  ANO_INICIAL,
+  VENDA_ANIMAIS_PRODUTOS_DESCRICOES,
+} from "@/utils/constants";
 
 export default function ReceitasTab({ data, isAdmin }) {
-  console.log(data);
-  const form = useForm();
   const [formsDisabled, setFormsDisabled] = useState(true);
   const [loading, setLoading] = useState(false);
+  const anos = Array.from({ length: 5 }, (_, index) => ANO_INICIAL + index);
+  const DESCRICOES = VENDA_ANIMAIS_PRODUTOS_DESCRICOES;
+
   const onEdit = () => {
     setFormsDisabled(false);
   };
+
   const handleCancel = () => {
     setFormsDisabled(true);
   };
+
   const onSave = async () => {
     console.log("enviando receitas");
   };
 
-  const anos = Array.from({ length: 5 }, (_, index) => ANO_INICIAL + index);
-
-  const receitas = [];
+  const handleInputChange = (descricao, ano, field, value) => {
+    console.log(`Changing ${field} for ${descricao} in year ${ano}:`, value);
+    // handle changes here
+  };
 
   return (
     <div className="p-4 bg-gray-900/80">
@@ -42,63 +49,63 @@ export default function ReceitasTab({ data, isAdmin }) {
         onCancel={handleCancel}
         isAdmin={isAdmin}
       />
-      <div className="w-full mt-4 sm:px-4 sm:py-2 flex flex-col gap-8">
-        <div className="h-screen">
-          <Table className="min-w-full border border-gray-300">
-            <TableHeader>
-              <TableRow className="">
-                <TableHead className="text-left py-2 border-r border-gray-300">
-                  DESCRIÇÃO
-                </TableHead>
-                <TableHead className="text-center py-2 border-r border-gray-300">
-                  UNID
-                </TableHead>
-                <TableHead className="text-center py-2 border-r border-gray-300">
-                  VALOR UNIT
-                </TableHead>
-                {anos.map((ano) => (
-                  <TableHead
-                    key={ano}
-                    className="text-center py-2 border-r border-gray-300"
-                  >
-                    {ano}
-                  </TableHead>
-                ))}
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {receitas.map((receita) => (
-                <TableRow key={receita.key}>
-                  <TableCell className="text-left border-t border-gray-300 py-2 px-4">
-                    {receita.label}
-                  </TableCell>
-                  <TableCell className="text-center border-t border-gray-300 py-2 px-4">
-                    {receita.unidade}
-                  </TableCell>
-                  <TableCell className="text-center border-t border-gray-300 py-2 px-4">
-                    <Input
-                      type="text"
-                      disabled={formsDisabled}
-                      className="text-center w-full bg-transparent border-none"
-                    />
-                  </TableCell>
-                  {anos.map((year) => (
-                    <TableCell
-                      key={year}
-                      className="text-center border-t border-gray-300 py-2 px-4"
-                    >
+      <div className="mt-4 grid grid-cols-1 gap-6">
+        {DESCRICOES.map((descricao, index) => (
+          <div
+            key={index}
+            className="border border-gray-300 p-4 rounded-lg bg-gray-800 shadow-md"
+          >
+            <h3 className="font-bold text-lg mb-4">{descricao}</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              {anos.map((ano, i) => (
+                <div
+                  key={ano}
+                  className="flex flex-col gap-2 bg-gray-700 p-5 rounded-lg"
+                >
+                  <div className="font-semibold text-center mb-4">
+                    Ano {ano}
+                  </div>
+                  <div className="flex gap-2">
+                    <div className="w-1/2">
+                      <label className="block text-sm mb-2"></label>
+                      <Input
+                        type="text"
+                        className="border-white"
+                        disabled={formsDisabled}
+                        placeholder="Qtd"
+                        onChange={(e) =>
+                          handleInputChange(
+                            descricao,
+                            ano,
+                            "qtd",
+                            e.target.value
+                          )
+                        }
+                      />
+                    </div>
+                    <div className="w-1/2">
+                      <label className="block text-sm mb-2"></label>
                       <Input
                         type="text"
                         disabled={formsDisabled}
-                        className="text-center w-full bg-transparent border-none"
+                        placeholder="Valor"
+                        className="border-white"
+                        onChange={(e) =>
+                          handleInputChange(
+                            descricao,
+                            ano,
+                            "valor",
+                            e.target.value
+                          )
+                        }
                       />
-                    </TableCell>
-                  ))}
-                </TableRow>
+                    </div>
+                  </div>
+                </div>
               ))}
-            </TableBody>
-          </Table>
-        </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
