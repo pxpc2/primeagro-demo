0c2f1058780bf5bc11ea8e07567bae847f4a5893
Pedro Daia <pedrodaia.c@gmail.com>
2024-10-02 21:04:49 -0300

    displaying despesas totais with colors

--- components/projeto/despesas-tab.jsx
+++ components/projeto/despesas-tab.jsx
@@ -1,8 +1,21 @@
+import { ANO_INICIAL } from "@/utils/constants";
 import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import Heading from "./Header";
 
 export default function DespesasTab({ data, isAdmin, receitasData }) {
-  console.log(receitasData);
+  const [loading, setLoading] = useState(false);
+  const [formsDisabled, setFormsDisabled] = useState(true);
+
   const [totalReceitas, setTotalReceitas] = useState(Array(12).fill(0));
+  const anos = Array.from({ length: 12 }, (_, index) => ANO_INICIAL + index);
 
   // Calcula os totais de Receitas para cada ano
   // ultimos dois anos copia ao 2033
@@ -35,14 +48,73 @@ export default function DespesasTab({ data, isAdmin, receitasData }) {
     setTotalReceitas(newTotalReceitas);
   }, []);
 
+  const handleCancel = () => {
+    setFormsDisabled(true);
+  };
+
+  const onEdit = () => {
+    setFormsDisabled(false);
+  };
+
+  const onSave = () => {
+    try {
+      setLoading(true);
+      setLoading(false);
+    } catch (e) {}
+  };
+
   return (
-    <div>
-      <h1>Total Receitas por Ano</h1>
-      {totalReceitas.map((total, index) => (
-        <p key={index}>
-          Total Receita for Year {2024 + index}: {total}
-        </p>
-      ))}
+    <div className="p-4 bg-gray-900/90">
+      <Heading
+        tabName={"Despesas"}
+        onEdit={onEdit}
+        onSave={onSave}
+        isEditing={!formsDisabled}
+        isLoading={loading}
+        onCancel={handleCancel}
+        isAdmin={isAdmin}
+      />
+      <div className="mt-8 mx-3">
+        <Table className="w-full border-collapse">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="border border-gray-500 p-1 text-sm font-semibold">
+                ANO
+              </TableHead>
+              {anos.map((ano) => (
+                <TableHead
+                  key={ano}
+                  className="border border-gray-500 p-1 text-sm"
+                >
+                  {ano}
+                </TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="border p-1 border-gray-500 text-sm font-semibold">
+                TOTAL RECEITAS
+              </TableCell>
+              {totalReceitas.map((total, index) => (
+                <TableCell
+                  key={index}
+                  className={`border p-1 text-right text-sm ${
+                    total < 0
+                      ? "border-red-500 text-red-500"
+                      : "border-green-500 text-green-500"
+                  }`}
+                >
+                  {total.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
